ae4b6062829dc3726d148c7149e308839568130b
Mikebulatov <mikebulatov@yandex.ru>
2023-08-06 01:43:35 +0700

    Задача 15. Изменение 1

--- zd15/Program.cs
+++ zd15/Program.cs
@@ -1,17 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Задача 6: Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).");
+Console.WriteLine("Задача 15: Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
 int num;
 Console.Write("Введите  число:");
 num = Convert.ToInt32(Console.ReadLine());
 
-if(num%2>0)
+if(num>7|| num <1)
 
 {
-    Console.WriteLine("Число не четное");
+    Console.WriteLine("Число не не соотвесвует дню недели");
 }
 else
 {
 
-    Console.WriteLine("Число четное");
-
+    if(num>5)Console.WriteLine("Входной день");
+    else Console.WriteLine("Будний день");
 }
