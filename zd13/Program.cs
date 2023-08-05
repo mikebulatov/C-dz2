eb3d90761457d03225bddf325ae689cd02eb9414
Mikebulatov <mikebulatov@yandex.ru>
2023-08-06 01:54:44 +0700

    Задача 13. Изменение 2

--- zd13/Program.cs
+++ zd13/Program.cs
@@ -1,13 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Задача 13: Программf, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.WriteLine("Задача 13: Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
 
 Console.Write("Введите число:");
 int num = Convert.ToInt32(Console.ReadLine());
 if(num >99)
 {
-string S=Convert.ToString(num);
-        Console.Write("3я цифра:");
-        Console.WriteLine(S[2]);
+        //string S=Convert.ToString(num);
+        ;
+        while (num>999)
+        num=num/10;
+       Console.Write("3я цифра:");
+       Console.WriteLine(num%10); 
+        //Console.WriteLine(S[2]);
 }
 else
 {
